81eee9162174229266b2b60f967ff48693fb3b81
Jason Kolbly <jason@rscheme.org>
2022-07-07 17:22:30 -0500

    Run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -14,19 +14,23 @@ use parse::parse;
 
 fn main() {
     println!("Hello, world!");
-    
-    let a = parse("module hello() -> (bit led) {
+
+    let a = parse(
+        "module hello() -> (bit led) {
         led[t] = ~led[t-1]; // Perform the bitwise NOT
-    }");
+    }",
+    );
     println!("{:#?}", a);
 
     //println!("{:?}", verify_node(&a, &HashSet::new()));
     //println!("{}", compile_ast(&a).unwrap());
     println!("{}", compile_module(&a));
 
-    let b = parse("module add(bit b, bit c) -> (bit a) {
+    let b = parse(
+        "module add(bit b, bit c) -> (bit a) {
         a[t] = a[t-1] + b[t] + c[t-1];
-    }");
+    }",
+    );
     println!("{:#?}", b);
 
     println!("{}", compile_module(&b));
