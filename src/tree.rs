87f28f5fab3daa40ab2610261045265a450ab251
Jason Kolbly <jason@rscheme.org>
2022-07-15 18:30:54 -0500

    Setup basic generic tree.rs

---
+++ src/tree.rs
@@ -0,0 +1,17 @@
+pub struct Tree<T> {
+    nodes: Vec<Node<T>>
+}
+
+pub struct Node<T> {
+    id: Option<NodeId>,
+    parent: Option<NodeId>,
+    previous_sibling: Option<NodeId>,
+    next_sibling: Option<NodeId>,
+    children: Option<Vec<NodeId>>,
+    data: T
+}
+
+pub struct NodeId {
+    //using usize guarantees vector of nodes is not too large
+    index: usize
+}
