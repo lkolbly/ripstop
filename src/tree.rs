e506c5187f8a4be4ab1205f0964560ec68b5e364
Jason Kolbly <jason@rscheme.org>
2022-07-18 18:47:15 -0500

    Use node ID's for tree editing methods

--- src/tree.rs
+++ src/tree.rs
@@ -1,3 +1,10 @@
+use std::fmt;
+
+#[derive(Debug, Clone)]
+pub enum TreeError {
+    NodeNotFound { node_id: NodeId },
+}
+
 pub struct Tree<T> {
     nodes: Vec<Node<T>>,
 }
@@ -7,15 +14,25 @@ impl<T> Tree<T> {
         Tree { nodes: Vec::new() }
     }
 
-    pub fn get_node(&mut self, id: &NodeId) -> Option<&Node<T>> {
-        self.nodes.get(id.index)
+    pub fn get_node(&self, id: &NodeId) -> Result<&Node<T>, TreeError> {
+        let ret = self.nodes.get(id.index);
+        if let None = ret {
+            Err(TreeError::NodeNotFound {
+                node_id: id.clone(),
+            })
+        } else {
+            Ok(ret.expect("Found to be not none."))
+        }
     }
 
-    pub fn get_node_option(&mut self, id: Option<NodeId>) -> Option<&Node<T>> {
-        if let Some(nodeid) = id {
-            self.get_node(&nodeid)
+    pub fn get_node_mut(&mut self, id: &NodeId) -> Result<&mut Node<T>, TreeError> {
+        let ret = self.nodes.get_mut(id.index);
+        if let None = ret {
+            Err(TreeError::NodeNotFound {
+                node_id: id.clone(),
+            })
         } else {
-            None
+            Ok(ret.expect("Found to be not none."))
         }
     }
 
@@ -24,27 +41,30 @@ impl<T> Tree<T> {
             index: self.nodes.len(),
         };
         self.nodes.push(Node {
-            id: id,
+            id,
             parent: None,
             previous_sibling: None,
             next_sibling: None,
             children: None,
-            data: data,
+            data,
         });
         id
     }
 
-    pub fn append_to(&mut self, node: &mut Node<T>, toappend: Node<T>) {
+    pub fn append_to(&mut self, nodeid: &NodeId, toappendid: &NodeId) -> Result<(), TreeError> {
+        let mut node = self.get_node_mut(nodeid)?;
+        let mut toappend = self.get_node_mut(toappendid)?;
+
         if let None = node.children {
             node.children = Some(Vec::new());
         }
 
-        let children = node.children.expect("Should never fail.");
+        let mut children = node.children.as_mut().expect("Should never fail.");
         if children.len() > 0 {
             let sibling_id = children
                 .get(children.len() - 1)
                 .expect("Should never fail.");
-            let sibling = self.get_node(sibling_id).expect("Should never fail.");
+            let mut sibling = self.get_node_mut(sibling_id)?;
 
             toappend.previous_sibling = Some(sibling_id.clone());
             sibling.next_sibling = Some(toappend.id);
@@ -52,25 +72,33 @@ impl<T> Tree<T> {
         children.push(toappend.id);
 
         toappend.parent = Some(node.id);
+
+        Ok(())
+    }
+}
+
+impl<T> fmt::Display for Tree<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "hello")
     }
 }
 
 pub struct Node<T> {
-    id: NodeId,
-    parent: Option<NodeId>,
-    previous_sibling: Option<NodeId>,
-    next_sibling: Option<NodeId>,
-    children: Option<Vec<NodeId>>,
-    data: T,
+    pub id: NodeId,
+    pub parent: Option<NodeId>,
+    pub previous_sibling: Option<NodeId>,
+    pub next_sibling: Option<NodeId>,
+    pub children: Option<Vec<NodeId>>,
+    pub data: T,
 }
 
 impl<T> Node<T> {
-    pub fn append(&mut self, toappend: Node<T>, tree: &mut Tree<T>) {
-        tree.append_to(self, toappend);
+    pub fn append(&mut self, toappend: &NodeId, tree: &mut Tree<T>) {
+        tree.append_to(&self.id, toappend);
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct NodeId {
     //using usize guarantees vector of nodes is not too large
     index: usize,
