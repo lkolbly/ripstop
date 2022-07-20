947b3da2ae6e3a6e41c6e91d4963f6d4b9534605
Jason Kolbly <jason@rscheme.org>
2022-07-20 12:15:40 -0500

    Okay fine I'll push ASTNode

--- src/ast.rs
+++ src/ast.rs
@@ -154,6 +154,35 @@ pub fn verify_ast(node: &mut Node) -> Result<(), Vec<VerificationError>> {
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum ASTNode {
+    //This is the head of a module. The code within a module is entirely children of the module
+    ModuleDeclaration {
+        id: String,
+        in_values: Vec<(Type, String)>,
+        out_values: Vec<(Type, String)>,
+    },
+    //Some examples of generated VariableReferences:
+    //my_var[t] => var_id: "my_var", t_offset: 0
+    //my_var[t - 10] => var_id: "my_var", t_offset: 10
+    //var_with_num8ers[t-2] => var_id: "var_with_num8ers", t_offset: 2
+    //my_var[t + 10] =x> this doesn't work, can't reference a future clock value
+    VariableReference {
+        var_id: String,
+        t_offset: i64,
+    },
+    //Unary operators only have one child
+    //Maybe extract operators into their own enum of sorts (or maybe just unary/binary ops)? Might not be helpful though
+    BitwiseInverse,
+    Add,
+    Subtract,
+    Assign,
+    VariableDeclaration {
+        var_type: Type,
+        var_id: String,
+    },
+}
+
 //Once the tree is generated, if any child has an invalid return type, it will return an error
 //For example, if the lhs and rhs of an assignment have different types, the compiler will return an error
 #[derive(Debug, Clone)]
