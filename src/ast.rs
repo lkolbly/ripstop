d93ead5724fdbd3ece3ac25cba1255010d612117
Bobxcat <ryan@rscheme.org>
2022-07-25 21:13:49 -0500

    Removed ast::Node

--- src/ast.rs
+++ src/ast.rs
@@ -156,41 +156,6 @@ pub enum ASTNode {
     },
 }
 
-//Once the tree is generated, if any child has an invalid return type, it will return an error
-//For example, if the lhs and rhs of an assignment have different types, the compiler will return an error
-#[derive(Debug, Clone)]
-pub enum Node {
-    //This is the head of a module. The code within a module is entirely children of the module
-    ModuleDeclaration {
-        id: String,
-        in_values: Vec<(Type, String)>,
-        out_values: Vec<(Type, String)>,
-    },
-    //Some examples of generated VariableReferences:
-    //my_var[t] => var_id: "my_var", t_offset: 0
-    //my_var[t - 10] => var_id: "my_var", t_offset: 10
-    //var_with_num8ers[t-2] => var_id: "var_with_num8ers", t_offset: 2
-    //my_var[t + 10] =x> this doesn't work, can't reference a future clock value
-    VariableReference {
-        var_id: String,
-        t_offset: i64,
-    },
-    //Unary operators only have one child
-    //Maybe extract operators into their own enum of sorts (or maybe just unary/binary ops)? Might not be helpful though
-    BitwiseInverse {},
-
-    //Binary operators have two children
-    Add {},
-    Subtract {},
-    Assign {},
-
-    VariableDeclaration {
-        var_type: Type,
-        var_id: String,
-    },
-    //TODO: Constants, [Some way of handling scope], etc.
-}
-
 //This ripstop code:
 /*
 module hello() -> (bit led) {
