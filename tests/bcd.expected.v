5f30864e73d207c36815bda7e843b1e53c914bb4
Lane Kolbly <lane@rscheme.org>
2022-08-06 11:56:52 -0500

    Add functional tests for bcd test case

--- tests/bcd.expected.v
+++ tests/bcd.expected.v
@@ -1,4 +1,7 @@
-module bcd_digit_add(
+module bcd(
+    input clk,
+    input rst,
+
     input[3:0] base,
     input[3:0] a,
     input[3:0] b,
@@ -10,8 +13,8 @@ module bcd_digit_add(
 
     wire[4:0] sum;
 
-    assign sum = {1'b0, a} + {1'b0, b};
-    assign carryout = sum > base;
+    assign sum = {1'b0, a} + {1'b0, b} + {3'b0, carryin};
+    assign carryout = sum >= base;
     assign c = carryout ? sum - base : sum;
 
 endmodule
