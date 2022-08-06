d8be78d4586ecb63f864107672526a6cffa122ad
Lane Kolbly <lane@rscheme.org>
2022-08-06 10:52:42 -0500

    Add parser to generate tests from CSV files

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,8 @@ import os
 import tempfile
 import shutil
 import subprocess
+import sys
+from mako.template import Template
 
 def get_files(pattern):
     files = os.listdir(".")
@@ -19,6 +21,9 @@ def pytest_generate_tests(metafunc):
 
     if "rp_codegen_testname" in metafunc.fixturenames:
         metafunc.parametrize("rp_codegen_testname", get_files(".compiled.v"))
+
+    if "rp_csvtestdata_testname" in metafunc.fixturenames:
+        metafunc.parametrize("rp_csvtestdata_testname", get_files(".test.csv"))
     pass
 
 class TempFile:
@@ -84,3 +89,81 @@ def test_correct_compiled_functionality(rp_functionality_testname):
 
     compiledf.rm()
     target.rm()
+
+def test_matches_csv(rp_csvtestdata_testname):
+    """
+    Tests that the compiled output matches the CSV data
+    """
+
+    test_harness = TempFile()
+
+    dut_inputs, dut_outputs, test_data = parse_test_data("{}.test.csv".format(rp_csvtestdata_testname))
+
+    with open(test_harness.filename, "w") as f:
+        generate_test_verilog({
+            "name": rp_csvtestdata_testname,
+            "inputs": dut_inputs,
+            "outputs": dut_outputs,
+        }, test_data, f)
+
+    compiledf = compile_rp(rp_csvtestdata_testname)
+    with open(compiledf.filename, "r") as f:
+        compiled = f.read()
+
+    with open(test_harness.filename, "r") as f:
+        test = f.read()
+
+    # Concatenate the compiled output and the test into a new file
+    target = TempFile()
+    with open(target.filename, "w") as t:
+        t.write(compiled)
+        t.write(test)
+
+    # Build that with iverilog
+    test_executable = TempFile()
+    subprocess.run("iverilog -o " + test_executable.filename + " " + target.filename, check=True, shell=True)
+
+    # Run the executable
+    stdout = subprocess.check_output(test_executable.filename, shell=True).decode("utf-8")
+    assert "fail" not in stdout, test_harness.filename
+
+    compiledf.rm()
+    target.rm()
+    test_harness.rm()
+
+def generate_test_verilog(dutspec, testdata, output):
+    result = Template(open("test.template.v.mako").read()).render(
+        dut=dutspec,
+        data=testdata,
+    )
+    output.write(result)
+
+def parse_test_data(testdata):
+    with open(testdata, "r") as f:
+        lines = f.read().split("\n")
+
+    header = lines[0]
+    fields = list(map(lambda column: column.strip(" \r\n").split(" "), header.split(",")))
+    inputs = list(map(lambda field: (field[0], int(field[2])), filter(lambda field: field[0] != "rst" and field[1] == "in", fields)))
+    outputs = list(map(lambda field: (field[0], int(field[2])), filter(lambda field: field[0] != "rst" and field[1] == "out", fields)))
+    fields = list(map(lambda field: field[0], fields))
+
+    data = []
+    for (rownum,row) in enumerate(lines[1:]):
+        row = row.split("//")[0].strip(" \r\n")
+        if len(row) == 0:
+            continue
+        values = list(map(lambda value: value.strip(" \r\n"), row.split(",")))
+        if len(values) != len(fields):
+            raise RuntimeError("Incorrect number of values on row " + str(rownum + 1))
+        row = {}
+        for k,v in zip(fields, values):
+            try:
+                v = int(v)
+            except:
+                if v != "x":
+                    raise RuntimeError("Unrecognized value of v on row {}, '{}'".format(rownum + 1, v))
+            row[k] = v
+        data.append(row)
+
+    return inputs, outputs, data
